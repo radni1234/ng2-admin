299390e82a9925b203a7858fe49bf7f8130cfbef
zeljko.zecevic <zeljko.zecevic@yahoo.com>
2016-12-12 14:13:50 +0100

    stepen dani

--- src/app/pages/admin/components/stepen_dani/stepen_dani.component.ts
+++ src/app/pages/admin/components/stepen_dani/stepen_dani.component.ts
@@ -4,7 +4,7 @@ import {LocalDataSource} from 'ng2-smart-table';
 import {CrudService} from '../../../services/crud.service';
 import {ViewChild} from "@angular/core/src/metadata/di";
 import {ModalDirective} from "ng2-bootstrap";
-import {StepenDan, Opstina, Godina} from "./stepen_danidata";
+import {StepenDan, Opstina, Godina, Mesec} from "./stepen_danidata";
 import {CompleterService, CompleterData, CompleterItem} from 'ng2-completer';
 
 @Component({
@@ -17,16 +17,24 @@ import {CompleterService, CompleterData, CompleterItem} from 'ng2-completer';
 export class StepenDaniComponent implements OnInit {
   @ViewChild('childModal') childModal: ModalDirective;
 
-  stepenDani: StepenDan[];
+  stepenDan: StepenDan = new StepenDan();
+  stepenDani: StepenDan[] = new Array<StepenDan>();
   selectedOpstina: Opstina;
   selectedGodina: Godina;
+  selectedMesec: Mesec;
+
   private dataServiceOpstina: CompleterData;
   private dataServiceGodina: CompleterData;
+  private dataServiceMesec: CompleterData;
   isOpstinaLoaded: boolean = false;
   isGodinaLoaded: boolean = false;
+  isMesecLoaded: boolean = false;
   opstinaLoaded = new EventEmitter<Opstina>();
   godinaLoaded = new EventEmitter<Godina>();
+  stepenDanaGodisnje: number;
   errorMessage:string;
+  IDSelectedOpstina: number;
+  IDSelectedGodina: number;
   jedinicaMere = {
     id: null,
     naziv: null,
@@ -53,6 +61,9 @@ export class StepenDaniComponent implements OnInit {
     mode: 'external',
     actions: {
       columnTitle: ''
+    },
+    pager: {
+      perPage: 12
     },
     noDataMessage: 'Podaci nisu pronađeni',
     columns: {
@@ -81,15 +92,46 @@ export class StepenDaniComponent implements OnInit {
   constructor(private crudService: CrudService, private fb: FormBuilder, private completerService: CompleterService) {
     this.myForm = this.fb.group({
       id: [''],
-      naziv: [''],
+      stependan: [''],
+      mesec: [''],
       version: ['']
     });
+  }
+  public onMesecSelected(selected: CompleterItem) {
+    console.log(selected);
+    if(selected!==null){
+      this.selectedMesec = selected.originalObject;
+    }
+
+  }
+
+  napuniMesece (){
+    this.crudService.getData("mesec")
+      .subscribe(
+        listaMeseca => {
+          console.log(listaMeseca);
+          this.dataServiceMesec = this.completerService.local(listaMeseca, 'naziv', 'naziv');
+          this.isMesecLoaded = true;
+        },
+        error => this.errorMessage = <any>error);
+
   }
   public onOpstinaSelected(selected: CompleterItem) {
     console.log(selected);
     if(selected!==null){
       this.selectedOpstina = selected.originalObject;
-      this.opstinaLoaded.emit(this.selectedOpstina);
+      this.IDSelectedOpstina = selected.originalObject.id;
+      this.stepenDanaGodisnje = 0;
+      this.source.empty();
+      while(this.stepenDani.length > 0) {
+        this.stepenDani.pop();
+      }
+      if(this.selectedGodina!=null){
+        console.log("EMITUJE OPSTINA AUTOOOOOOOOOOOOOOOOO");
+        this.opstinaLoaded.emit(this.selectedOpstina);
+      }
+
+
     }
 
   }
@@ -109,9 +151,23 @@ export class StepenDaniComponent implements OnInit {
   public onGodinaSelected(selected: CompleterItem) {
     console.log(selected);
     if(selected!==null){
+
+      this.IDSelectedGodina = selected.originalObject.id;
       this.selectedGodina = selected.originalObject;
-      console.log("GGGGGGGGGGGGGGGGGGGGGGGG" + this.selectedGodina.id);
-      this.godinaLoaded.emit(this.selectedGodina);
+      console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      console.log(this.selectedGodina);
+      this.stepenDanaGodisnje = 0;
+      this.source.empty();
+      while(this.stepenDani.length > 0) {
+        this.stepenDani.pop();
+      }
+      if(this.selectedOpstina!=null){
+        console.log("EMITUJE GODINA AUTOOOOOOOOOOOOOOOOO");
+        console.log("ID GODINE "+this.selectedGodina.id);
+        this.godinaLoaded.emit(this.selectedGodina);
+      }
+
+
     }
 
   }
@@ -127,53 +183,66 @@ export class StepenDaniComponent implements OnInit {
         error => this.errorMessage = <any>error);
 
   }
+  napuniStepenDane(){
+    this.crudService.getData("stepen_dan").subscribe(
+      data => {
+
+
+        for(var sd of data){
+          // console.log("U Petlji");
+          // console.log("ID GODINE U PETLJI " + sd.godina.id);
+          // console.log("ID SELEKTOVANE GODINE " + this.selectedGodina.id);
+
+
+          if(sd.godina.id == this.IDSelectedGodina && sd.opstina.id == this.IDSelectedOpstina){
+            console.log("UPECANNNNNNNNNNNNNNNNNNNNNNNNNNNNN");
+            this.stepenDani.push(sd);
+            console.log(sd.sdIznos);
+            this.stepenDanaGodisnje += sd.sdIznos;
+          }
+        }
+        //this.stepenDani = data;
+        this.source.load(this.stepenDani);
+        console.log(data);
+      },
+      error => console.log(error)
+    );
+  }
 
   getData() {
 
     this.opstinaLoaded.subscribe(
       selopstina => {
-        console.log("OPSTINA SELEKTOVANA");
-        this.godinaLoaded.subscribe(
-          selgodina => {
-            console.log("GODINA SELEKTOVANA");
-            this.crudService.getData("stepen_dan").subscribe(
-              data => {
+          this.napuniStepenDane();
 
-                console.log("ID SELEKTOVANE GODINE" + selopstina.id);
-                for(var sd of data){
-                  console.log("U Petlji");
-                  console.log("ID GODINE U PETLJI" + sd.godina.id);
-
-
-                  if(sd.godina.id==selopstina.id && sd.opstina.id ==selgodina.id){
-                    console.log("UPECANNNNNNNN");
-                    this.stepenDani.push(sd);
-                  }
-                }
-                //this.stepenDani = data;
-                this.source.load(data);
-                console.log(data);
-              },
-              error => console.log(error)
-            );
-          },
-          error => this.errorMessage = <any>error);
       },
-      error => this.errorMessage = <any>error);
+      error => console.log(error)
+    );
+
+    this.godinaLoaded.subscribe(
+      selgodina => {
+        this.napuniStepenDane();
+
+      },
+      error => console.log(error)
+    );
 
 
   }
 
   naliranje() {
-    this.jedinicaMere.id = null;
-    this.jedinicaMere.naziv = null;
-    this.jedinicaMere.version = null;
+    this.stepenDan.id = null;
+    this.stepenDan.opstina = null;
+    this.stepenDan.mesec = null;
+    this.stepenDan.sdIznos = null;
+    this.stepenDan.version = null;
   }
 
   ngOnInit() {
     this.getData();
     this.napuniOpstine();
     this.napuniGodine();
+    this.napuniMesece();
   }
 
   onCreate(): void{
@@ -182,9 +251,14 @@ export class StepenDaniComponent implements OnInit {
   }
 
   onEdit(event): void{
-    this.jedinicaMere = event.data;
+    this.naliranje();
+    this.stepenDan = event.data;
+//    this.selectedMesec = this.stepenDan.mesec;
+    console.log("GGGGGGGGGGGGGGGGGGGGGG");
+    console.log(this.stepenDan);
     this.izbor = true;
-    this.source.setFilter([{ field: 'naziv', search: '' }]);
+
+
   }
 
   onCancel() {
@@ -192,11 +266,25 @@ export class StepenDaniComponent implements OnInit {
     this.izbor = false;
   }
 
-  onSubmit(objekat) {
+  onSubmit() {
+    console.log(this.selectedOpstina);
+    this.stepenDan.opstina = this.selectedOpstina;
+    this.stepenDan.godina = this.selectedGodina;
+    this.stepenDan.mesec = this.selectedMesec;
+    this.stepenDanaGodisnje = 0;
 
-    this.crudService.sendData("stepen_dan", objekat)
+    console.log(this.stepenDan);
+
+    this.crudService.sendData("stepen_dan", this.stepenDan)
       .subscribe(
-        data => {console.log(data); this.getData();},
+        data => {
+          console.log(data);
+          this.source.empty();
+          while(this.stepenDani.length > 0) {
+            this.stepenDani.pop();
+          }
+          this.napuniStepenDane();
+        },
         error => console.log(error)
       );
 
@@ -212,7 +300,15 @@ export class StepenDaniComponent implements OnInit {
   onDeleteConfirm() {
     this.crudService.delete("stepen_dan", this.brisanjeId)
       .subscribe(
-        data => {console.log(data); this.getData();},
+        data => {
+          console.log(data);
+          this.source.empty();
+          while(this.stepenDani.length > 0) {
+            this.stepenDani.pop();
+          }
+          this.stepenDanaGodisnje = 0;
+          this.napuniStepenDane();
+        },
         error => console.log(error)
       );
 
