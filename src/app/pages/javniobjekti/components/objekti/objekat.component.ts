8ac71b8041ae069d82e07932da7d230b43f46cbd
nikola.vujovic <vujovicnidzo@yahoo.com>
2016-12-22 19:49:47 +0100

    finance

--- src/app/pages/javniobjekti/components/objekti/objekat.component.ts
+++ src/app/pages/javniobjekti/components/objekti/objekat.component.ts
@@ -1,16 +1,22 @@
 import {Component, OnInit, ViewEncapsulation} from "@angular/core";
-import {Stavka} from "./objekatfindata";
+import {Stavka, Kamate, StartneVrednosti} from "./objekatfindata";
 // webpack html imports
 let template = require('./objekat.componet.html');
 
 @Component({
   selector: 'accordion-demo',
+  styles: [require('./objekat.scss')],
   template: template
 })
 export class ObjekatComponent implements OnInit{
   public oneAtATime:boolean = true;
   public items:Array<string> = ['Item 1', 'Item 2', 'Item 3'];
-  private stavke: Stavka[];
+  private stavka: Stavka;
+  private stavke: Stavka[] = new Array<Stavka>();
+  private kamate: Kamate = new Kamate();
+  private start: StartneVrednosti = new StartneVrednosti();
+  private brojGodina: number = 10;
+  private ucitan: boolean = false;
 
   public status:Object = {
     isFirstOpen: true,
@@ -29,23 +35,38 @@ export class ObjekatComponent implements OnInit{
   ];
 
   public izracunaj() {
+    while(this.stavke.length>0){
+      this.stavke.pop();
+    }
     var Finance = require('financejs');
-
     var finance = new Finance();
-
-
-    // To calculate Amortization
-
     console.log(finance.AM(20000, 7.5, 5, 0));
+    console.log(2**4);
 
-    // => 400.76
 
+
+    for(var i = 0; i<this.brojGodina; i++){
+      this.stavka = new Stavka ();
+      this.stavka.rbGodina = i+1;
+      this.stavka.prihodEnergent = this.start.startniPrihodEnergent * ((1 + this.kamate.kamPrihodEnergent/100)**(i+1));
+      this.stavka.trosakEnergent = this.start.startniTrosakEnergent * ((1 + this.kamate.kamTrosakEnergent/100)**(i+1));
+      console.log(this.stavka);
+      console.log(i);
+      this.stavke.push(this.stavka);
+      this.stavka = null;
+    }
+    console.log(this.stavke);
+    this.ucitan = true;
   }
 
   public addItem():void {
     this.items.push(`Items ${this.items.length + 1}`);
   }
   ngOnInit(){
+    this.start.startniPrihodEnergent = 100000;
+    this.start.startniTrosakEnergent = 70000;
+    this.kamate.kamPrihodEnergent = 2;
+    this.kamate.kamTrosakEnergent = 2;
     this.izracunaj();
   }
 }
--- src/app/pages/javniobjekti/components/objekti/objekatfindata.ts
+++ src/app/pages/javniobjekti/components/objekti/objekatfindata.ts
@@ -3,3 +3,13 @@ export class Stavka {
   trosakEnergent: number;
   prihodEnergent: number;
 }
+
+export class Kamate {
+  kamTrosakEnergent: number = 0.02;
+  kamPrihodEnergent: number = 0.02;
+}
+
+export class StartneVrednosti {
+  startniTrosakEnergent: number = 70000;
+  startniPrihodEnergent: number = 100000;
+}
