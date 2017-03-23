f12b6c63c26c63aeb5ec07554f9eb1f419d9df0f
zeljko.zecevic <zeljko.zecevic@yahoo.com>
2017-03-23 11:23:26 +0100

    selection tool

--- src/app/pages/izvestaji/components/aps_mes_pot/aps_mes_pot.component.ts
+++ src/app/pages/izvestaji/components/aps_mes_pot/aps_mes_pot.component.ts
@@ -3,6 +3,7 @@ import {Component, ViewEncapsulation, OnInit} from "@angular/core";
 import {CrudService} from "../../../services/crud.service";
 import {Objekat} from "../../../javniobjekti/components/objekti/objekatdata";
 import { IMultiSelectTexts, IMultiSelectSettings, IMultiSelectOption } from 'angular-2-dropdown-multiselect/src/multiselect-dropdown';
+import {unescape} from "querystring";
 declare let jsPDF : any;
 
 @Component({
@@ -271,4 +272,19 @@ export class IzvApsMesPot implements OnInit {
   //
   //     this.datumOd = '01.01.'+yyyy;
   // }
+  tableToExcel(table, name) {
+    console.log("bbbbbbbbbbbbbbbbbbb");
+    var uri = 'data:application/vnd.ms-excel;base64,'
+      , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+      , base64 = function(s) {
+      console.log("cccccccccccccccccccccccc");
+      return window.btoa(unescape(encodeURIComponent(s))) }
+      , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+    return function(table, name) {
+      console.log("aaaaaaaaaaaaaaaaaaaaaa");
+      if (!table.nodeType) table = document.getElementById(table)
+      var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+      window.location.href = uri + base64(format(template, ctx))
+    }
+  }
 }
