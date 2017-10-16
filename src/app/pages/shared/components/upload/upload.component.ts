ac85cb963d74b292458a41faaaf7dfd590973fb4
radni4321 <radni4321@yahoo.com>
2017-10-16 15:50:35 +0200

    upload slike - id

--- src/app/pages/shared/components/upload/upload.component.ts
+++ src/app/pages/shared/components/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import {Http, Headers, Response} from '@angular/http';
+import {Http, Headers, Response, URLSearchParams, RequestOptions, ResponseContentType} from '@angular/http';
 import {AuthenticationService} from "../../../services/authentication.service";
 
 @Component({
@@ -8,6 +8,7 @@ import {AuthenticationService} from "../../../services/authentication.service";
 })
 export class UploadComponent {
   @Input() multiple: boolean = false;
+  @Input() objekatId: number;
   @ViewChild('fileInput') inputEl: ElementRef;
   private headers: Headers;
 
@@ -22,12 +23,22 @@ export class UploadComponent {
     if (fileCount > 0) { // a file was selected
       for (let i = 0; i < fileCount; i++) {
         formData.append('file', inputEl.files.item(i));
+        formData.append('id', this.objekatId);
       }
 
     // if (fileCount = 1) { // a file was selected
     //   formData.append('file', inputEl.files.item(0));
 
       this.headers = new Headers();
+      // const params = new URLSearchParams();
+      // params.set('id', id.toString());
+
+      // const options = new RequestOptions({
+      //   headers: this.headers,
+      //   responseType: ResponseContentType.Json,
+      //   params: null,
+      //   withCredentials: false
+      // });
 
       // this.headers.append('Content-Type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
       // this.headers.append('Content-Disposition', 'form-data; name="file"; filename="model.png"');
@@ -36,7 +47,7 @@ export class UploadComponent {
 
       return this.http.post('http://178.222.245.73:8090/upload/post', formData, {
         headers: this.headers
-      }).subscribe(
+           }).subscribe(
           data => console.log(data),
           err => console.log(err))
         ;
