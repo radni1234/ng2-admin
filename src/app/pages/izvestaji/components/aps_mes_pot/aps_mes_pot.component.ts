d7788cf8209a406626b326b3cd8518cbdb28e706
Zeljko <zeljko.zecevic@yahoo.com>
2018-01-17 12:26:46 +0100

    dodatak 1

--- src/app/pages/izvestaji/components/aps_mes_pot/aps_mes_pot.component.ts
+++ src/app/pages/izvestaji/components/aps_mes_pot/aps_mes_pot.component.ts
@@ -264,7 +264,7 @@ export class IzvApsMesPot implements OnInit {
 
     doc.save('ApsMesPot'+this.selektovaniObjekat+'.pdf');
   }
-//funkcija za formiranje excela iz
+//funkcija za formiranje excela iz izvestaja
   htmlTableToExcel(table) {
     var uri = 'data:application/vnd.ms-excel;base64,'
       , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
