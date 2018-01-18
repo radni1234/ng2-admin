754c73b2b707077d4795b438c01ea56696592d93
radni1234 <radni1234@yahoo.com>
2018-01-18 10:43:09 +0100

    rasveta izvestaji - dorada

--- src/app/pages/javnarasveta/izvestaji/godisnja_potrosnja/godisnja_potrosnja.component.ts
+++ src/app/pages/javnarasveta/izvestaji/godisnja_potrosnja/godisnja_potrosnja.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
-import {MonthYearPicker} from "../../../shared/components/month_year_picker/month_year_picker.component";
 import {CrudService} from "../../../services/crud.service";
 import {RasvetaGodisnjaPotrosnja} from "./godisnja_potrosnja.data";
+import {YearPicker} from "../../../shared/components/year_picker/year_picker.component";
 
 @Component({
   selector: 'izvestaj_rasveta_godisnja_potrosnja',
   templateUrl: 'godisnja_potrosnja.component.html'
 })
 export class IzvRasGodPotComponent implements OnInit {
-  @ViewChild(MonthYearPicker)
-  private m: MonthYearPicker;
+  @ViewChild(YearPicker)
+  private m: YearPicker;
 
   trafoiId: any[];
 
@@ -29,7 +29,7 @@ export class IzvRasGodPotComponent implements OnInit {
 
   formirajIzvestaj(){
 
-      this.crudService.getData("izvestaj/ras_god_pot?trafo_id="+this.trafoiId+"&datum_od="+'15'+'.'+this.m.mesOd+'.'+this.m.godOd+"&datum_do="+'15'+'.'+this.m.mesDo+'.'+this.m.godDo).subscribe(
+      this.crudService.getData("izvestaj/ras_god_pot?trafo_id="+this.trafoiId+"&datum_od="+'01.01.'+this.m.godOd+"&datum_do="+'31.12.'+this.m.godDo).subscribe(
         data => {this.podaci = data; console.log(data);},
         error => {console.log(error);}
       );
