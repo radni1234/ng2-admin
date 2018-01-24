6674997e20798693cb383f97f3e15466648ea690
radni1234! <radni1234@yahoo.com>
2018-01-24 09:08:36 +0100

    excel resen

--- src/app/pages/izvestaji/components/aps_mes_pot/aps_mes_pot.component.ts
+++ src/app/pages/izvestaji/components/aps_mes_pot/aps_mes_pot.component.ts
@@ -264,14 +264,15 @@ export class IzvApsMesPot implements OnInit {
 
     doc.save('ApsMesPot'+this.selektovaniObjekat+'.pdf');
   }
+
 //funkcija za formiranje excela iz izvestaja 7
   htmlTableToExcel(table) {
     var uri = 'data:application/vnd.ms-excel;base64,'
       , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
       , base64 = function(s) {return btoa(encodeURIComponent(s).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-      return String.fromCharCode("0x" + p1.toNumber());
-    }));
-      , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }); };
+      return String.fromCharCode(parseInt("0x" + p1));
+    })); }
+    , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }); };
 
      if (!table.nodeType) table = document.getElementById(table);
      var ctx = { worksheet: "aps-mes-pot" || 'Worksheet', table: table.innerHTML };
