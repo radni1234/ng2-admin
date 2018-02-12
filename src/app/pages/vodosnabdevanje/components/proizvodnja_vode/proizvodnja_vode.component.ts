554818e0477d1744545f6e8a49ebe1f1541cc9fb
radni1234 <radni1234@yahoo.com>
2018-02-12 11:01:34 +0100

    vodozahvat - izmene

--- src/app/pages/vodosnabdevanje/components/proizvodnja_vode/proizvodnja_vode.component.ts
+++ src/app/pages/vodosnabdevanje/components/proizvodnja_vode/proizvodnja_vode.component.ts
@@ -6,6 +6,9 @@ import {Router} from "@angular/router";
 import {ModalDirective} from "ng2-bootstrap";
 import {ProizvodnjaVode} from "./proizvodnja_vode.data";
 import {Vodozahvat} from "../vodozahvat/vodozahvat.data";
+import {DatePipe} from "@angular/common";
+import {MesecLista} from "../../../javniobjekti/components/racuni/racundata";
+import {DatumService} from "../../../services/datum.service";
 
 
 @Component({
@@ -26,10 +29,34 @@ export class ProizvodnjaVodeComponent implements OnInit  {
 
   myForm: FormGroup;
 
-  datum: string;
-  myDatePickerOptions = {
-    dateFormat: 'dd.mm.yyyy'
-  };
+  provera: any = 0;
+  datePipe = new DatePipe();
+
+  godine: number [] = new Array <number>();
+  godina: number;
+  brojGodinaUMeniju: number = 10;
+
+  mesec: MesecLista = new MesecLista();
+  meseci: Array<any> = [ {"id":0, "naz":"Januar"},
+    {"id":1, "naz":"Februar"},
+    {"id":2, "naz":"Mart"},
+    {"id":3, "naz":"April"},
+    {"id":4, "naz":"Maj"},
+    {"id":5, "naz":"Jun"},
+    {"id":6, "naz":"Jul"},
+    {"id":7, "naz":"Avgust"},
+    {"id":8, "naz":"Septembar"},
+    {"id":9, "naz":"Oktobar"},
+    {"id":10, "naz":"Novembar"},
+    {"id":11, "naz":"Decembar"}
+  ];
+
+  stariMesec: number;
+  staraGodina: number;
+
+  datumRacuna: Date = new Date();
+
+  noviRn: boolean = false;
 
   settings = {
     add: {
@@ -47,8 +74,12 @@ export class ProizvodnjaVodeComponent implements OnInit  {
     },
     noDataMessage: 'Podaci nisu pronađeni',
     columns: {
-      datum: {
-        title: 'Datum',
+      godina: {
+        title: 'Godina',
+        type: 'string'
+      },
+      mesec: {
+        title: 'Mesec',
         type: 'string'
       },
       proizvodnja: {
@@ -58,10 +89,12 @@ export class ProizvodnjaVodeComponent implements OnInit  {
     }
   };
 
-  constructor(private crudService: CrudService, private fb: FormBuilder, private router: Router) {
+  constructor(private crudService: CrudService, private fb: FormBuilder, private ds: DatumService, private router: Router) {
     this.myForm = this.fb.group({
       id: [''],
       datum: [''],
+      godina: [''],
+      mesec: [''],
       proizvodnja: [''],
       version: ['']
     });
@@ -72,7 +105,7 @@ export class ProizvodnjaVodeComponent implements OnInit  {
   }
 
   getData() {
-    this.crudService.getData("proizvodnja_vode/sve?vodozahvat_id=" + this.vodozahvat.id).subscribe(
+    this.crudService.getData("proizvodnja_vode/tab?vodozahvat_id=" + this.vodozahvat.id).subscribe(
       data => {this.source.load(data); console.log(data);},
       error => {console.log(error); this.router.navigate(['/login']);}
     );
@@ -81,6 +114,8 @@ export class ProizvodnjaVodeComponent implements OnInit  {
   onCreate(): void{
     this.proizvodnjaVode = new ProizvodnjaVode();
     this.proizvodnjaVode.vodozahvat = this.vodozahvat;
+    this.popuniGodinaMesec(new Date());
+    this.noviRn = true;
     this.izbor = true;
   }
 
@@ -88,8 +123,17 @@ export class ProizvodnjaVodeComponent implements OnInit  {
     this.proizvodnjaVode = new ProizvodnjaVode();
     this.crudService.getSingle("proizvodnja_vode/jedan?id="+event.data.id).subscribe(
       data => {this.proizvodnjaVode = data;
-        this.datum = this.proizvodnjaVode.datum;
+        // this.datum = this.proizvodnjaVode.datum;
         console.log(data);
+
+        this.datumRacuna = this.ds.toDate(this.proizvodnjaVode.datum);
+
+        this.popuniGodinaMesec(this.datumRacuna);
+
+        this.stariMesec = this.datumRacuna.getMonth();
+        this.staraGodina = this.datumRacuna.getFullYear();
+
+        this.noviRn = false;
         this.izbor = true;
       },
       error => {console.log(error); });
@@ -103,13 +147,18 @@ export class ProizvodnjaVodeComponent implements OnInit  {
   }
 
   onSubmit() {
+    this.proizvodnjaVode.datum = this.datePipe.transform(this.datumRacuna, 'dd.MM.yyyy');
+
     this.crudService.sendData("proizvodnja_vode", this.proizvodnjaVode)
       .subscribe(
-        data => {console.log(data); this.getData();},
+        data => {console.log(data);
+                this.getData();
+                },
         error => console.log(error)
       );
 
     this.izbor = false;
+    this.noviRn = false;
     this.proizvodnjaVode = new ProizvodnjaVode();
   }
 
@@ -136,8 +185,58 @@ export class ProizvodnjaVodeComponent implements OnInit  {
     this.childModal.hide();
   }
 
-  onDateChangedDatum(event:any) {
-    console.log('onDateChanged(): ', event.date, ' - formatted: ', event.formatted, ' - epoc timestamp: ', event.epoc);
-    this.proizvodnjaVode.datum = event.formatted;
+  napuniGodine(){
+    let datum = new Date();
+    let godina = datum.getFullYear();
+    for(var i = 0; i < this.brojGodinaUMeniju; i++){
+      this.godine.push(godina - i);
+    }
+
+  }
+
+  public onGodinaSelected(selectedGodina: number){
+    this.provera = 0;
+    this.datumRacuna.setFullYear(selectedGodina);
+
+    if (this.noviRn || (!this.noviRn && (this.godina != this.staraGodina || this.mesec.id != this.stariMesec))) {
+      this.proveriRacun("proizvodnja_vode/provera?datum="+this.datePipe.transform(this.datumRacuna, 'dd.MM.yyyy')+"&vodozahvat_id="+this.vodozahvat.id);
+    }
+
+  }
+
+  public onMesecSelected(selectedMesec: number){
+    this.provera = 0;
+    this.datumRacuna.setMonth(selectedMesec);
+    this.datumRacuna.setDate(15);
+
+    if (this.noviRn || (!this.noviRn && (this.godina != this.staraGodina || this.mesec.id != this.stariMesec))) {
+      this.proveriRacun("proizvodnja_vode/provera?datum="+this.datePipe.transform(this.datumRacuna, 'dd.MM.yyyy')+"&vodozahvat_id="+this.vodozahvat.id);
+    }
+
+  }
+
+  popuniGodinaMesec(datum: Date){
+    this.mesec.id = datum.getMonth();
+
+    if(this.godine.length==0) {
+      this.napuniGodine();
+    }
+
+    for (var item of this.godine) {
+      if (item == datum.getFullYear()) {
+        this.godina = item;
+      }
+    }
+  }
+
+  proveriRacun(url: string){
+
+    this.crudService.getData(url).subscribe(
+      data => {
+        this.provera = data;
+      },
+      error => console.log(error)
+    );
+
   }
 }
